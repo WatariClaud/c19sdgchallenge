395efbc3fd43fbb89161a1029a4ba8e0dc69c654
Claud Watari <claudwatari95@gmail.com>
2020-04-08 02:11:58 +0300

    add-new-functions

--- src/estimator.js
+++ src/estimator.js
@@ -1,40 +1,41 @@
+const input = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+let impact = {};
+let severeImpact = {};
+
+const estimateCurrentlyInfected = () => {
+  impact.currentlyInfected = input.reportedCases * 10;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
+  return impact;
+};
+const estimateExpectedToBeInfected = () => {
+  severeImpact.currentlyInfected = input.reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
+  return severeImpact;
+};
+
 const covid19ImpactEstimator = (data) => {
-  const inputData = {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
-    },
-    periodType: 'days',
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-  };
-  data.inputData = inputData;
+  const estimator = chain (
+    estimateCurrentlyInfected,
+    estimateExpectedToBeInfected,
+  );
 
-  const outputData = {
-    data,
-    impact: {},
-    severeImpact: {}
-  };
-
-  const calculateEstimatedInfections = () => {
-    let impactInfected = outputData.impact.currentlyInfected;
-    let severeInfected = outputData.severeImpact.currentlyInfected;
-    impactInfected = inputData.reportedCases * 10;
-    severeInfected = inputData.reportedCases * 50;
-    outputData.impact.infectionsByRequestedTimeDays = impactInfected * 1024;
-    outputData.severeImpact.infectionsByRequestedTimeDays = severeInfected * 1024;
-    outputData.impact.infectionsByRequestedTimeWeeks = outputData.impact.infectionsByRequestedTimeDays * 7;
-    outputData.severeImpact.infectionsByRequestedTimeWeeks = outputData.severeImpact.infectionsByRequestedTimeDays * 7;
-    outputData.impact.infectionsByRequestedTimeMonths= outputData.impact.infectionsByRequestedTimeWeeks * 4;
-    outputData.severeImpact.infectionsByRequestedTimeMonths= outputData.severeImpact.infectionsByRequestedTimeWeeks* 4;
-
-    return outputData;
-  };
-  calculateEstimatedInfections();
+  return estimator({
+      data,
+      impact: {},
+      severeImpact: {}
+  });
 };
 
 export default covid19ImpactEstimator;
