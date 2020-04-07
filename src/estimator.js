5ca4c07e98541e5299e01749a0aa7bbe4013592d
Claud Watari <claudwatari95@gmail.com>
2020-04-08 00:54:19 +0300

    challenge1-beta

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const inputData = {
+        region: {
+            name: "Africa",
+            avgAge: 19.7,
+            avgDailyIncomeInUSD: 5,
+            avgDailyIncomePopulation: 0.71
+        },
+        periodType: "days",
+        timeToElapse: 58,
+        reportedCases: 674,
+        population: 66622705,
+        totalHospitalBeds: 1380614
+    }, outputData = {
+        data: {},
+        impact: {},
+        severeImpact: {}
+    };
+
+    let impactInfected = outputData.impact.currentlyInfected;
+    let severeInfected = outputData.severeImpact.currentlyInfected;
+    impactInfected = reportedCases & 10;
+    severeInfected = reportedCases * 50;
+
+    let impactInfectionsByTime = outputData.impact.infectionsByRequestedTime;
+    let severeInfectionsByTime = outputData.severeImpact.infectionsByRequestedTime;
+    impactInfectionsByTime = impactInfected * 1024;
+    severeInfectionsByTime = severeInfected * 1024;
+};
 
 export default covid19ImpactEstimator;
